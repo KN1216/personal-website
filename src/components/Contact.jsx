35b57a2cb7a7fe8ee15985cb174f7f7b419835f7
KN1216 <kelvinokocha777@gmail.com>
2025-09-13 23:14:43 -0400

    resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,11 @@
+import resume from "../assets/KelvinOkochaResume.pdf";
 function Contact () {
     return (
         <>
             <h1 id="contact" className="text-white font-wix font-bold text-7xl">CONTACT</h1>
             <h3 className="text-white mt-5">Currently available for career opportunities! Shoot me an email at:{" "}
             <a href="mailto:kelvinokocha777@gmail.com" className="underline font-bold">kelvinokocha777@gmail.com</a></h3>
-            <h3 className="text-white font-bold underline">Resume</h3>
+            <h3 className="text-white font-bold underline"><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></h3>
         </>
     );
 }
