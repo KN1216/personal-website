3e2099309ff0b32a0be8e7d55df6c757eb6886fa
KN1216 <kelvinokocha777@gmail.com>
2025-09-25 16:12:30 -0400

    added linkedin link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ function Contact () {
             <h3 className="text-white mt-5">Currently available for career opportunities! Shoot me an email at:{" "}
             <a href="mailto:kelvinokocha777@gmail.com" className="underline font-bold">kelvinokocha777@gmail.com</a></h3>
             <h3 className="text-white font-bold underline"><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></h3>
+            <h3 className="text-white font-bold underline"><a href="https://www.linkedin.com/in/kelvin-okocha/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h3>
+
         </>
     );
 }
